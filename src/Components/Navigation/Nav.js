1383686261e704e5ab19b9c1f07a7e11346ca1a6
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2017-11-16 23:49:02 +0000

    Version 1
    Files loaded so I can look for feedback on Slack

---
+++ src/Components/Navigation/Nav.js
@@ -0,0 +1,20 @@
+// navigation menu component
+
+import React from 'react';
+
+// Imports the NavLink Component which can recognise the current url
+// and allows links to be styles as active
+import { NavLink } from 'react-router-dom';
+
+// Returns the Nav buttons when the Component is rendered in the app
+const Nav = props => (
+  <nav className="main-nav">
+    <ul>
+      <li><NavLink to='/cats'>Cats</NavLink></li>
+      <li><NavLink to='/dogs'>Dogs</NavLink></li>
+      <li><NavLink to='/computers'>Computers</NavLink></li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
